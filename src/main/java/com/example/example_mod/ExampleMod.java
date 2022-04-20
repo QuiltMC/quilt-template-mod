0a9b240074f2d2d0ad40a9b2e58754bf9f17ed75
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2022-04-20 16:23:53 -0300

    Apply suggestions from code review
    Co-authored-by: Emma C. Pointer-Null <emmaffle@modrinth.com>

--- src/main/java/com/example/example_mod/ExampleMod.java
+++ src/main/java/com/example/example_mod/ExampleMod.java
@@ -7,7 +7,7 @@ import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
+	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("Example Mod");
 
